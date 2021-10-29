09c4cd9f4dd6212008d7119e70c9b3e0b848408b
SNguyen <84067158+SonNguyen25@users.noreply.github.com>
2021-10-29 19:31:52 -0400

    Add files via upload

--- Assignment 4/src/ImagePPM.java
+++ Assignment 4/src/ImagePPM.java
@@ -1,4 +1,27 @@
-public class ImagePPM {
-  private int[][] image; //pixel class?
+import java.util.ArrayList;
+import java.util.List;
 
+public class ImagePPM implements Image {
+  private int width;
+  private int height;
+  private List<int[][]> rgb;
+
+  public ImagePPM(String filename) {
+    this.rgb = new ArrayList<int[][]>();
+  }
+
+  @Override
+  public int readImage() {
+    return 0;
+  }
+
+  @Override
+  public int width() {
+    return this.width;
+  }
+
+  @Override
+  public int height() {
+    return this.height;
+  }
 }
