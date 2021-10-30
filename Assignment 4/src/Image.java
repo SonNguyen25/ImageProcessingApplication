79f9d3d7ccf6f1e3d159c474dd1d9c2e2d551ec8
SNguyen <84067158+SonNguyen25@users.noreply.github.com>
2021-10-30 17:35:05 -0400

    Add files via upload

--- Assignment 4/src/Image.java
+++ Assignment 4/src/Image.java
@@ -1,4 +1,10 @@
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
 public interface Image {
+
+  public void readImage(String filename) throws FileNotFoundException;
 
   int width();
 
--- Assignment 4/src/ImagePPM.java
+++ Assignment 4/src/ImagePPM.java
@@ -1,19 +1,55 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class ImagePPM implements Image {
   private int width;
   private int height;
   private List<int[][]> rgb;
 
-  public ImagePPM(String filename) {
-    this.rgb = new ArrayList<int[][]>();
+  public ImagePPM(String filename) throws FileNotFoundException {
+//    this.rgb = new ArrayList<int[][]>();
+    this.readImage(filename);
   }
 
   @Override
-  public int readImage() {
-    return 0;
+  public void readImage(String filename) throws FileNotFoundException {
+    Scanner sc;
+    sc = new Scanner(new FileInputStream(filename));
+    StringBuilder builder = new StringBuilder();
+    //read the file line by line, and populate a string. This will throw away any comment lines
+    while (sc.hasNextLine()) {
+      String s = sc.nextLine();
+      if (s.charAt(0) != '#') {
+        builder.append(s + System.lineSeparator());
+      }
+    }
+    //now set up the scanner to read from the string we just built
+    sc = new Scanner(builder.toString());
+    String token;
+    token = sc.next();
+    if (!token.equals("P3")) {
+      System.out.println("Invalid PPM file: plain RAW file should begin with P3");
+    }
+    this.width = sc.nextInt();
+    this.height = sc.nextInt();
+    int maxValue = sc.nextInt();
+    int[][] red = new int[height][width];
+    int[][] green = new int[height][width];
+    int[][] blue = new int[height][width];
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        red[i][j] = sc.nextInt();
+        green[i][j] = sc.nextInt();
+        blue[i][j] = sc.nextInt();
+      }
+    }
+    this.rgb = new ArrayList<int[][]>(Arrays.asList(red, green, blue));
   }
+
 
   @Override
   public int width() {
@@ -24,4 +60,5 @@ public class ImagePPM implements Image {
   public int height() {
     return this.height;
   }
+
 }
