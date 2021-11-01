6d76cd3289d22a80b89c8f14b4e290b800960754
SNguyen <84067158+SonNguyen25@users.noreply.github.com>
2021-11-01 03:38:03 -0400

    Add files via upload

---
+++ Assignment 4/src/test/AbstractModelTest.java
@@ -0,0 +1,12 @@
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class AbstractModelTest<T> {
+
+  @Test
+  public void brighten() {
+    
+  }
+}
---
+++ Assignment 4/src/test/PPMModelTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+
+public class PPMModelTest {
+
+
+  @org.junit.Test
+  public void brighten() {
+  }
+
+  @org.junit.Test
+  public void flip() {
+  }
+}
