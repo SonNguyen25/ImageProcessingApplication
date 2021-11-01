48164166a1f8d8c5a7f14e52096c20f98ea9ccbc
SNguyen <84067158+SonNguyen25@users.noreply.github.com>
2021-11-01 16:36:41 -0400

    Add files via upload

---
+++ Assignment 4/src/model/storage/ImageLibrary.java
@@ -0,0 +1,24 @@
+package model.storage;
+
+import model.image.ImageModel;
+
+/**
+ * This interface represents the operations offered by an image storage.
+ * One object of the model represents one storage of image stored in the program.
+ */
+public interface ImageLibrary {
+
+  /**
+   * Put an image into the hashmap to store it.
+   * @param key represents the name of the image.
+   * @param model represents the model of the image.
+   */
+  void put(String key, ImageModel model);
+
+  /**
+   * Remove an image from the storage.
+   * @param key represents the name of the image.
+   * @param model represents the model of the image.
+   */
+  void remove(String key, ImageModel model);
+}
