c972a4eeb5d45b760eb6797bbda320a8c772e434
SNguyen <84067158+SonNguyen25@users.noreply.github.com>
2021-11-02 18:36:39 -0400

    Delete src directory

--- src/command/Loader/Loader.java
+++
@@ -1,7 +0,0 @@
-package command.Loader;
-
-import model.image.ImageModel;
-
-public interface Loader<T> {
-  ImageModel load() throws IllegalStateException;
-}
--- src/view/ImageView.java
+++
@@ -1,9 +0,0 @@
-package view;
-
-import java.io.IOException;
-
-public interface ImageView {
-  String toString(String fileName);
-
-  void renderMessage(String message) throws IOException;
-}
