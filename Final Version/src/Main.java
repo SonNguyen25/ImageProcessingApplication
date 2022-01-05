88c09fed879135c9e695ec56fb86e5a9d08fd252
SNguyen <84067158+SonNguyen25@users.noreply.github.com>
2022-01-05 05:01:02 -0500

    Delete Final Version directory

--- Final Version/src/Main.java
+++
@@ -1,9 +0,0 @@
-//public class Main {
-//  public static void main(String[] args) {
-//    int x = 0;
-//    double xx = Math.ceil((0 + 0.0) * (2 + 0.0) / (2 + 0.0));
-//    double xxx = Math.floor((0 + 0.0) * (2 + 0.0) / (2 + 0.0));
-//    System.out.println("" + xx + " " + xxx);
-//  }
-//}
-
